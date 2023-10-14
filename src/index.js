a29b6925a17a1795506517bab3173f6d701722aa
Faiyaz Kazi <faiyazk1012@gmail.com>
2023-10-13 22:48:16 -0400

    AR Chess

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.js'; // Replace './App' with the correct path to your main component
-import './index.css';
+import './index.css'; 
 
 ReactDOM.render(
     (<BrowserRouter>
             <App />
     </BrowserRouter>), 
-    document.getElementById('root'));
+    document.getElementById('root')); 
